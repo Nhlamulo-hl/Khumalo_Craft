437892a70608ab2636f717b891015b98f6c1b56c
Nhlamulo-hl <ST10107399@rcconnect.edu.za>
2024-04-04 16:14:40 +0200

    files

---
+++ Pages/ContactUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Khumalo_Crafts.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/MyWork.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Khumalo_Crafts.Pages
+{
+    public class MyWorkModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
